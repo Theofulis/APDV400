f1ac1ac7818c0b48174c88968186a54c482b49b7
Theofulis <philusmashego@gmail.com>
2020-07-17 12:53:46 +0200

    The java code here adds two number (int data type) and print the sum as output.

---
+++ src/main/java/com/mycompany/mashego201749548/Mashego201749548.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.mashego201749548;
+
+/**
+ *
+ * @author theophilus
+ */
+public class Mashego201749548{
+    
+    
+    public static void main(String[] args) {
+        
+        int num1 = 5;
+        int num2 = 9;
+        
+        System.out.println("The sum of 5 and 9 = " + (num1 + num2));
+    }
+    
+}
+
